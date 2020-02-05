201d71bd47594882603e515de1cfdf888b576141
adam139 <yuejun.tang@gmail.com>
2020-02-05 10:01:38 +0800

    add weixin zhifu and test it

---
+++ xtcs/policy/browser/zhifu.py
@@ -0,0 +1,72 @@
+#-*- coding: UTF-8 -*-
+from plone.memoize.instance import memoize
+from Products.Five.browser import BrowserView
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from my315ok.wechat.interfaces import IwechatSettings
+from my315ok.wechat.lib import WeixinHelper 
+
+from xtcs.policy import _
+from xtcs.policy.browser.interfaces import IThemeSpecific
+
+
+
+oauth2url = 'https://open.weixin.qq.com/connect/oauth2/authorize'
+
+class ZhiFuWeiXinAuthView(BrowserView):
+        
+    @memoize
+    def outputjs2(self):
+        "用户同意授权，获取code"
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IwechatSettings)
+        if bool(settings.appid):
+            appid = settings.appid
+        else:
+            appid = "wx2833460b1571bd01"            
+        if bool(settings.redirecturi):
+            redirecturi = settings.redirecturi
+        else:
+            redirecturi = 'http://www.xtcs.org'
+                          
+        out = """
+        function init() {
+        var ruri = encodeURIComponent(%(ruri)s);
+        var oauthUrl = '%(au2url)s?appid=%(appid)s&redirect_uri=' + ruri + '&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect';
+        window.location.href = oauthUrl;
+        }
+        init();
+        """ % dict(ruri=redirecturi,appid=appid,au2url=oauth2url)
+
+        return out        
+
+    @memoize
+    def outputjs(self):
+        "用户同意授权，获取code"
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IwechatSettings)
+        if bool(settings.appid):
+            appid = settings.appid
+        else:
+            appid = "wx2833460b1571bd01"            
+        if bool(settings.redirecturi):
+            redirecturi = settings.redirecturi
+        else:
+            redirecturi = 'http://weixin.315ok.org/static/pay.html'
+        nexturl = WeixinHelper.oauth2(redirecturi)
+                          
+        out = """
+        function init() {
+         window.location.href = %(oauthUrl)s;
+        }
+        init();
+        """ % dict(oauthUrl=nexturl)
+
+        return out
+    
+class ZhiFuWeiXinPayView(BrowserView):
+    "fetch access_token by code,"
+    pass 
+    
+               
+ 
---
+++ xtcs/policy/tests/test_zhifu_view.py
@@ -0,0 +1,80 @@
+#-*- coding: UTF-8 -*-
+import json
+import hmac
+from hashlib import sha1 as sha
+from plone.keyring.interfaces import IKeyManager
+from Products.CMFCore.utils import getToolByName
+from xtcs.policy.setuphandlers import STRUCTURE,_create_content 
+from xtcs.policy.testing import FunctionalTesting
+from plone.app.testing import TEST_USER_ID, login, TEST_USER_NAME, \
+    TEST_USER_PASSWORD, setRoles
+from plone.testing.z2 import Browser
+import unittest
+from zope.component import getUtility
+
+
+class TestView(unittest.TestCase):
+    
+    layer = FunctionalTesting
+    def setUp(self):
+        portal = self.layer['portal']
+        setRoles(portal, TEST_USER_ID, ('Manager',))                                                                                                                        
+        self.portal = portal
+    
+    def test_zhifu_weixin_auth_view(self):
+
+        app = self.layer['app']
+        portal = self.layer['portal']       
+        browser = Browser(app)
+        browser.handleErrors = False
+        browser.addHeader('Authorization', 'Basic %s:%s' % (TEST_USER_NAME, TEST_USER_PASSWORD,))       
+
+        import transaction
+        transaction.commit()
+        obj = portal.absolute_url() + '/@@auth.html'    
+        browser.open(obj)
+ 
+        outstr = "wx2833460b1571bd01"
+        import pdb
+        pdb.set_trace()
+        self.assertTrue(outstr in browser.contents)
+        
+    def test_zhifu_weixin_workflow_view(self):
+
+        app = self.layer['app']
+        portal = self.layer['portal']       
+        browser = Browser(app)
+        browser.handleErrors = False
+        browser.addHeader('Authorization', 'Basic %s:%s' % (TEST_USER_NAME, TEST_USER_PASSWORD,))       
+
+
+        from zope.interface import alsoProvides
+        from xtcs.policy.interfaces import IJuanzengworkflow
+        alsoProvides(portal,IJuanzengworkflow)
+        import transaction
+        transaction.commit()        
+        obj = portal.absolute_url() + '/@@donated_workflow?code=2&mail=3'    
+        browser.open(obj)
+ 
+        outstr = "wx2833460b1571bd01"
+        self.assertTrue(outstr in browser.contents)
+        
+        
+    def test_ajax_search(self):
+        request = self.layer['request']        
+        keyManager = getUtility(IKeyManager)
+        secret = keyManager.secret()
+        auth = hmac.new(secret, TEST_USER_NAME, sha).hexdigest()
+        request.form = {
+                        '_authenticator': auth,
+                        'fee': 10,
+                        'body':'test' ,
+                        'openid':'oQ61n01gs3t34TglBy_x2U6l8VWk',                                                                                                                    
+                        }
+# Look up and invoke the view via traversal
+        view = self.portal.restrictedTraverse('@@pay_ajax')
+        result = view()
+
+
+        self.assertEqual(json.loads(result),10)         
+  
