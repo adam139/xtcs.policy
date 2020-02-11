49ab333fe20f694452382ccb240a6eca326d0e82
adam139 <yuejun.tang@gmail.com>
2020-02-11 15:58:28 +0800

    fix table define

--- xtcs/policy/sql/init.sql
+++ xtcs/policy/sql/init.sql
@@ -13,19 +13,20 @@ create table if not exists onlinepay (
     money decimal(13,2) not null default 0.00,
     goods varchar(64) not null default '',
     atime DATETIME not null DEFAULT CURRENT_TIMESTAMP,
-    openid varchar(32) not null default 'noweixin',
+    openid varchar(128) not null default 'noweixin',
     status tinyint(1) not null  default 0,
     index pay_openid(openid),
     index pay_name(aname)
     
 ) engine=InnoDB DEFAULT CHARSET=utf8;
 
--- openid accesstoken cache table for weixin
+-- openid accesstoken cache table for weixin jsapi
+-- every user has self openid and accesstoken
 
 create table if not exists accesstoken (
     id integer unsigned not null auto_increment primary key,
-    openid varchar(32) not null default 'weixindefaultopenid',
-    token varchar(128) not null default 'weixindefaultaccesstoken',
+    openid varchar(128)  UNIQUE not null default 'weixindefaultopenid',
+    token varchar(128) not null default 'weixindefaultaccesstoken', 
     index weixin_openid(openid)
     
 ) engine=InnoDB DEFAULT CHARSET=utf8;
