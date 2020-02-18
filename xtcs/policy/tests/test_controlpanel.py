e7187c0a753b3c4fe197a8087bf0fc7d155e1e95
adam139 <yuejun.tang@gmail.com>
2020-02-18 13:30:20 +0800

    add controlpanel test

--- xtcs/policy/tests/test_controlpanel.py
+++ xtcs/policy/tests/test_controlpanel.py
@@ -8,6 +8,7 @@ from xtcs.policy.testing import FunctionalTesting,POLICY_INTEGRATION_TESTING
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 from xtcs.policy.browser.interfaces import IwechatSettings
+from my315ok.wechat.config import ControlPanelConf_pub 
 
 class TestSetup(unittest.TestCase):
     
@@ -18,10 +19,12 @@ class TestSetup(unittest.TestCase):
     def test_setting_configured(self):
         registry = getUtility(IRegistry)
         settings = registry.forInterface(IwechatSettings)
-        self.assertEqual(settings.appid, "wx77d2frty25808f911")
-        self.assertEqual(settings.redirecturi, "http://www.xtcs.org/")
+#         self.assertEqual(settings.appid, "wx77d2frty25808f911")
+#         self.assertEqual(settings.preview, "http://www.xtcs.org/")
+        st = ControlPanelConf_pub()
         import pdb
-        pdb.set_trace()
+        pdb.set_trace()        
+        self.assertEqual(st.CURL_TIMEOUT, 40)      
 
         self.assertEqual(settings.access_token_time, datetime.strptime("2014-08-14 00:00:00", '%Y-%m-%d %H:%M:%S'))
 
