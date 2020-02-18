cc44c23be39239e697b0441d70883a260ccf24c5
adam139 <yuejun.tang@gmail.com>
2020-02-18 10:13:24 +0800

    fix can't fetch number from wuzi Chinese string

--- xtcs/policy/copy_table.py
+++ xtcs/policy/copy_table.py
@@ -73,7 +73,7 @@ def cp_donor2juanzeng(context):
             meta = MetaData(engine)
             src_table = Table('donor', meta, autoload=True)
             stm = select([src_table.c.did, src_table.c.aname, src_table.c.money, src_table.c.goods, src_table.c.atime])\
-            .where(src_table.c.aname !=u"").order_by(asc(src_table.c.did)).limit(size).offset(offset)
+            .where(src_table.c.aname != u"").order_by(asc(src_table.c.did)).limit(size).offset(offset)
             rs = con.execute(stm)
         return rs.fetchall()
     
@@ -83,40 +83,46 @@ def cp_donor2juanzeng(context):
     idmap = {7:6,12:7,13:8,18:9,21:10,22:11}
     import re
     
-    for j in recorders:
+    for rdr in recorders:
         data = {}
-        data['xiangmu_id'] = idmap[int(j[0])]
-        data['xingming'] = j[1].strip()
-        xianjin = j[2]
-        if bool(xianjin): 
-            if xianjin.isdecimal():               
-                data['xianjin'] = float(xianjin)
-        wuzi = j[3]
+        data['xiangmu_id'] = idmap[int(rdr[0])]
+        data['xingming'] = rdr[1].strip()
+        xianjin = rdr[2]
+        try:
+            xianjin = float(xianjin)
+            data['xianjin'] = xianjin
+        except:
+            data['xianjin'] = 0.00
+        wuzi = rdr[3]
         if bool(wuzi):
             wz = wuzi.split(',')
-            if wuzi.isdecimal():               
-                data['wuzi_jiazhi'] = float(wuzi)
-                data['wuzi'] = "价值{0}元物资".format(wuzi).encode('utf-8')        
-             
-            elif len(wz) > 1:
-                wuzijiazhi =  re.findall(r'\d+',wz[-1])
-                if bool(wuzijiazhi):
-                    data['wuzi_jiazhi'] = float(wuzijiazhi[0])
-                    data['wuzi'] = wuzi                
-
-            elif len(wz) == 1:
-                s5 = u"\uff0c"
-                wz= wuzi.split(s5)
-                if len(wz) > 1:                
-                    wuzijiazhi =  re.findall(r'\d+',wz[-1])
+            try:
+                wjiazhi = float(wuzi)                         
+                data['wuzi_jiazhi'] = wjiazhi
+                data['wuzi'] = "价值{0}元物资".format(wjiazhi).encode('utf-8')
+            except:
+                if len(wz) > 1: 
+                    wuzijiazhi = re.findall(r'\d+',wz[-1])
                     if bool(wuzijiazhi):
                         data['wuzi_jiazhi'] = float(wuzijiazhi[0])
                         data['wuzi'] = wuzi
-                else:                        
-                    data['wuzi'] = wuzi        
+                elif len(wz) == 1:
+                    s5 = u"\uff0c"
+                    wz= wuzi.split(s5)
+                    if len(wz) > 1:                
+                        wuzijiazhi =  re.findall(r'\d+',wz[-1])
+                        if bool(wuzijiazhi):
+                            data['wuzi_jiazhi'] = float(wuzijiazhi[0])
+                            data['wuzi'] = wuzi
+                    else:                        
+                        data['wuzi'] = wuzi
+                        data['wuzi_jiazhi'] = 0.00        
+        else:
+            data['wuzi'] = ""
+            data['wuzi_jiazhi'] = 0.00 
         data['status'] = 1
-        shijian = j[4]
-
+        data['openid'] = 'noweixin'
+        shijian = rdr[4]
         if bool(shijian) and isinstance(shijian,datetime):
             data['juanzeng_shijian'] = shijian
         elif isinstance(shijian,int):
@@ -124,8 +130,8 @@ def cp_donor2juanzeng(context):
         else:
             data['juanzeng_shijian'] = datetime.strptime("2000-01-01 00:00:00",fmt)                                          
         try:
-#             pass
-            locator.add(data)
+            locator.add(data)            
+            continue
         except:
             continue
     return                         
