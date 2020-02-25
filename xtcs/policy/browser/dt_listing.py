485fe982c212724900c92dfb8f5b6aa90e4ac188
adam139 <yuejun.tang@gmail.com>
2020-02-25 17:07:50 +0800

    add  datatable style table

---
+++ xtcs/policy/browser/dt_listing.py
@@ -0,0 +1,13 @@
+#-*- coding: UTF-8 -*-
+from Products.Five.browser import BrowserView
+from Products.CMFPlone.resources import add_resource_on_request
+
+
+class JuanZengView(BrowserView):
+    """using datatable"""
+    
+    def __init__(self,context, request):
+        # Each view instance receives context and request as construction parameters
+        self.context = context
+        self.request = request
+        add_resource_on_request(self.request, 'mockup-patterns-datatables')    
