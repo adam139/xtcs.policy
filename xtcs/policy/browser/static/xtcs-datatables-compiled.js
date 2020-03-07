284b610135c3b502fdc88827087a5aba5f3b81e9
adam139 <yuejun.tang@gmail.com>
2020-03-07 20:45:36 +0800

    add url parameters analys function

--- xtcs/policy/browser/static/xtcs-datatables-compiled.js
+++ xtcs/policy/browser/static/xtcs-datatables-compiled.js
@@ -19734,8 +19734,37 @@ require([
   'mockup-patterns-datatables' 
 ], function($) {
   'use strict';
+// read url query string
+$.extend({
+  getUrlVars: function(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name){
+    return $.getUrlVars()[name];
+  }
+});  
+  
 $(document).ready(function(){
  var ajax_url = $("#ajaxsearch").attr('data-ajax-target');
+ var byName = $.getUrlVar('name');
+ if (byName === undefined || byName == null || byName === "") { 	 	
+  } else {
+  	var byName2 = decodeURIComponent(byName);
+  	$(".documentFirstHeading").html(byName2);
+  }
+ var byId = $.getUrlVar('id');  
+  if (byId === undefined || byId == null || byId === "") {  	
+  } else {
+  	$("#xiangmuId").val(byId);
+  }
  $('#datatable').DataTable({
  	"processing": true,
  	"serverSide": true,
