c5e28d7f011984f7e0cae918894e3924f28aa282
adam139 <yuejun.tang@gmail.com>
2020-03-07 19:48:37 +0800

    recomplie bundle

--- xtcs/policy/browser/static/xtcs-datatables-compiled.js
+++ xtcs/policy/browser/static/xtcs-datatables-compiled.js
@@ -19734,8 +19734,22 @@ require([
   'mockup-patterns-datatables' 
 ], function($) {
   'use strict';
-$(document).ready(function(){ 
- $('#datatable').DataTable();
+$(document).ready(function(){
+ var ajax_url = $("#ajaxsearch").attr('data-ajax-target');
+ $('#datatable').DataTable({
+ 	"processing": true,
+ 	"serverSide": true,
+ 	"ajax":{
+ 		'url':ajax_url,
+ 		'type':'POST',
+        "data": function ( d ) {                
+                 d.xiangmu_id = $('#xiangmuId').val();                
+            } 		
+ 	},
+ 	'language':{
+ 		'url':'http://cdn.datatables.net/plug-ins/1.10.20/i18n/Chinese.json'
+ 	},
+ });
 	});
 });
 
